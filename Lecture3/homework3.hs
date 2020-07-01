65a48d5068ade5361359dbd6eec05139e68ac5bf
Logan Clark <logan.d.clark@slu.edu>
2020-06-30 23:55:05 -0500

    Add solutions to exercise 2
    Solved exercise 2 and added notes about what mistakes I made at first,
    and made note on exercise 1

--- Lecture3/homework3.hs
+++ Lecture3/homework3.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
 ---- Exercise 1
+---- I'm sure nth can be shorter, maybe using filter somehow
 
 nth :: [a] -> Int -> Int -> [a]
 nth [] _ _ = []
@@ -10,3 +11,33 @@ nth (x:xs) k n
 
 skips :: [a] -> [[a]]
 skips x = map (nth x 1) [1 .. (length x)]
+
+-- Exercise 2
+-- Local maxima
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs)
+  | x < y && z < y = y:(localMaxima (z:xs))
+  | True = localMaxima (y:z:xs)
+localMaxima _ = []
+
+{-
+I started this one by dealing with the empty, single, and double element lists
+first, like
+
+localMaxima [] = []
+localMaxima [_] = []
+localMaxima [_,_] = []
+localMaxima (x:...) = ...
+
+But then I realized that with pattern matching, I could deal with all these cases
+at once just by shifting the main case to the top and using the wildcard to deal
+with the remaining issues.
+-}
+
+-- Exercise 3
+histogram :: [Integer] -> String
+
+
+
+------------------ EOF
